79e17ced6b6dffdbedc69e2e5e6ffd4f5f80043f
Tomáš Kocifaj <kocifaj@ktstudio.cz>
2014-12-29 15:33:09 +0100

    Přidáné objektu pro správu vlastních URL adres KT_Custom_Template a KT_Template_Registrator

---
+++ core/classes/custom_url/kt_custom_template.inc.php
@@ -0,0 +1,185 @@
+<?php
+
+class KT_Custom_Template {
+
+    private $pageName;
+    private $title;
+    private $fileName;
+    private $templatePath;
+
+    /**
+     * Založení nového Custom URL
+     *
+     * @author Tomáš Kocifaj
+     * @link http://www.KTStudio.cz
+     *
+     * @param string $pageName - bude volán soubor z adresy $path/$pagename.php
+     * @param string $path - musí existovat
+     * @param string $title - titulek stránky v HTML
+     * @return \KT_Custom_Template
+     * @throws KT_Not_Set_Argument_Exception
+     */
+    public function __construct($pageName, $fileName, $path, $title = '') {
+        $this->setPageName($pageName);
+
+        if (kt_isset_and_not_empty($path)) {
+            $this->setTemplatePath($path);
+        } else {
+            throw new KT_Not_Set_Argument_Exception('path');
+        }
+
+        $this->setFileName($fileName);
+
+        if (kt_isset_and_not_empty($title)) {
+            $this->setTitle($title);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPageName() {
+        return $this->pageName;
+    }
+
+    /**
+     * Nastaví název stránky - slug v url
+     * 
+     * @author Tomáš Kocifaj
+     * @link http://www.KTStudio.cz
+     * 
+     * @param string $pageName
+     * @return \KT_Custom_Template
+     */
+    public function setPageName($pageName) {
+        $this->pageName = $pageName;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle() {
+        return $this->title;
+    }
+
+    /**
+     * Nastaví HTML titulek pro danou templatu
+     *
+     * @author Tomáš Kocifaj
+     * @link http://www.KTStudio.cz
+     *
+     * @param type $title
+     * @return \KT_Custom_Template
+     * @throws KT_Not_Set_Argument_Exception
+     */
+    public function setTitle($title) {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFileName() {
+        return $this->fileName;
+    }
+
+    /**
+     * Nastaví název souboru, který identifikuje script, který se bude pro zobrazení stránky volat
+     * 
+     * @author Tomáš Kocifaj
+     * @link http://www.ktstudio.cz
+     * 
+     * @param type $fileName
+     * @return \KT_Custom_Template
+     * @throws InvalidArgumentException
+     */
+    public function setFileName($fileName) {
+        $file = $this->getFullFilePath();
+        if (is_file($file)) {
+            $this->fileName = $fileName;
+            return $this;
+        } else {
+            $this->fileName = '';
+            throw new InvalidArgumentException(__("Tento soubor $file neexistuje!", KT_DOMAIN));
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    function getTemplatePath() {
+        return $this->templatePath;
+    }
+
+    /**
+     * Nastaví cestu na příslušnou templatu - musí existovat
+     *
+     * @author Tomáš Kocifaj
+     * @link http://www.KTStudio.cz
+     *
+     * @param type $path
+     * @return \KT_Custom_Template
+     * @throws InvalidArgumentException
+     * @throws KT_Not_Set_Argument_Exception
+     */
+    public function setTemplatePath($path) {
+        if (kt_not_isset_or_empty($path)) {
+            throw new KT_Not_Set_Argument_Exception('path');
+        }
+
+        if (!is_dir($path)) {
+            throw new InvalidArgumentException(__("Tato cesta $path neexistuje!", KT_DOMAIN));
+        }
+
+        $this->templatePath = $path;
+        return $this;
+    }
+
+    // --- protected funkce ------------------
+
+    /**
+     * Vrátí celou cestu k templatě $path + $pagename.php
+     *
+     * @author Tomáš Kocifaj
+     * @link http://www.KTStudio.cz
+     *
+     * @return type
+     * @throws InvalidArgumentException
+     */
+    protected function getFullFilePath() {
+        if (is_dir($this->templatePath)) {
+            $fileName = "{$this->fileName}.tmp.php";
+            return path_join($this->templatePath, $fileName);
+        }
+        throw new InvalidArgumentException("template_path doesn't exist");
+    }
+
+    // --- statické funkce ------------------
+
+    public static function getPermalink($pagename, $id = null) {
+        global $wp_rewrite;
+        $baseUrl = get_bloginfo('url');
+        if ($wp_rewrite->permalink_structure) {
+            if (kt_isset_and_not_empty($id)) {
+                $url = $baseUrl . "/{$pagename}/{$id}";
+            } else {
+                $url = $baseUrl . "/{$pagename}";
+            }
+        } else {
+            if (kt_isset_and_not_empty($id)) {
+                $url = $baseUrl . "/?pagename={$pagename}&id={$id}";
+            } else {
+                $url = $baseUrl . "/?pagename={$pagename}";
+            }
+        }
+
+        return $url;
+    }
+
+}
---
+++ core/classes/custom_url/kt_template_registrator.inc.php
@@ -0,0 +1,133 @@
+<?php
+
+class KT_Template_Registrator {
+
+    private $urls = array();
+
+    /**
+     * Založí nové rewrite rules do WP domena.cz/pagename
+     * Po každém přidání nutné aktualizovat permalinks Nastaveni -> Trvalá Odkazy ->Uložit změny
+     *
+     * @author Tomáš Kocifaj
+     * @link http://www.KTStudio.cz
+     *
+     */
+    public function __construct() {
+        add_filter("rewrite_rules_array", array($this, "register"));
+        add_filter("template_include", array($this, "getCustomTemplate"));
+    }
+
+    /**
+     * Přidá nové URL do WP front-endu
+     *
+     * @param string $pagename
+     * @param string $title
+     * @return \KT_Template_Registrator
+     * @throws KT_Not_Set_Argument_Exception
+     */
+    public function addUrl($pagename, $filename, $path, $title = '') {
+        if (kt_isset_and_not_empty($pagename)) {
+            $url = new KT_Custom_Template($pagename, $filename, $path, $title);
+            $this->urls[$url->getPageName()] = $url;
+            return $this;
+        }
+        throw new KT_Not_Set_Argument_Exception($pagename);
+    }
+
+    /**
+     * Zavede všechny definované URL do rewrite rules
+     *
+     * @author Tomáš Kocifaj
+     * @link http://www.KTStudio.cz
+     *
+     * @param type $aRules
+     * @return type
+     */
+    public function register($aRules) {
+
+        $aNewRules = array();
+
+        foreach ($this->urls as $url) {
+            $pageName = $url->getPageName();
+            $aNewRules[] = array("$pageName/?$" => "index.php?pagename=$pageName");
+        }
+
+        foreach ($aNewRules as $val) {
+            $aRules = $val + $aRules;
+        }
+
+        return $aRules;
+    }
+
+    /**
+     * Akce vrátí do WP Front-endu příslušnou templatu z uvedené path URL objektu
+     *
+     * @author Tomáš Kocifaj
+     * @link http://www.KTStudio.cz
+     *
+     * @global WP_Query $wp_query
+     * @param string $template
+     * @return string
+     */
+    public function getCustomTemplate($template) {
+        global $wp_query;
+        $pagename = $wp_query->query_vars["pagename"];
+
+        if (kt_not_isset_or_empty($pagename)) {
+            return $template;
+        }
+
+        if (!in_array($pagename, array_keys($this->urls))) {
+            return $template;
+        }
+
+        $wp_query->is_404 = '';
+        $wp_query->is_kt_template = 1;
+
+        $url = $this->getUrlObject($pagename);
+
+        $kt_template = $url->getFullFilePath();
+
+        if (is_file($kt_template)) {
+            add_filter('wp_title', array($this, 'customUrlTitle'), 10, 1);
+            return $kt_template;
+        }
+
+        return $template;
+    }
+
+    /**
+     * Vrátí URL Object KT_Custom_Template na základě registrovaného pagename
+     *
+     * @author Tomáš Kocifaj
+     * @link http://www.KTStudio.cz
+     *
+     * @param string $name
+     * @return KT_Custom_Template
+     * @throws KT_Not_Set_Argument_Exception
+     */
+    public function getUrlObject($name) {
+        if (kt_isset_and_not_empty($this->urls[$name])) {
+            return $this->urls[$name];
+        }
+        throw new KT_Not_Set_Argument_Exception('name');
+    }
+
+    /**
+     * Funkce pro zavedení HTML titulku pro custom page - nutné pro wp_title filter
+     *
+     * @author Tomáš Kocifaj
+     * @link http://www.KTStudio.cz
+     *
+     * @global WP_Query $wp_query
+     * @param string $title
+     * @return string
+     */
+    public function customUrlTitle($title) {
+        global $wp_query;
+        $pagename = $wp_query->query_vars["pagename"];
+        $url = $this->getUrlObject($pagename);
+        return $pageTitle = $url->getTitle();
+    }
+
+}
