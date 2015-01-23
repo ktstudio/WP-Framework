f3a1af9fdd250da9f2e6e5018b5848970aee69c6
Martin Hlaváč <hlavacm@hotmail.com>
2015-01-23 16:45:34 +0100

    Rozšíření KT_Shortcode_Base o možnost zadat tlačítko do editoru
    resp. o možnost zadat klíč a cestu ke skriptu, na základě kterých se pak
    provede případná automatická registrace...

--- core/classes/kt_shortcode_base.inc.php
+++ core/classes/kt_shortcode_base.inc.php
@@ -9,22 +9,29 @@
 abstract class KT_Shortcode_Base implements KT_Registrable {
 
     private $tag;
+    private $buttonKey;
+    private $buttonScriptPath;
 
     /**
      * Vytvoření nového shortcodu s (povinnými) parametry
+     * Pozn. registrace tlačítka v editoru je možná, jen když je zadán klíč i cesta skriptu pro tlačítko v editoru
      *
      * @author Martin Hlaváč
      * @link http://www.ktstudio.cz
      *
      * @param string $tag
      */
-    function __construct($tag) {
+    function __construct($tag, $buttonKey = null, $buttonScriptPath = null) {
         if (kt_isset_and_not_empty($tag)) {
             $this->tag = $tag;
         } else {
             throw new KT_Not_Set_Argument_Exception("tag");
         }
+        $this->buttonKey = $buttonKey;
+        $this->buttonScriptPath = $buttonScriptPath;
     }
+
+    // --- getry & setry ---------------------------
 
     /**
      * Vrátí název tagu, resp. shortcodu pod kterým je definovám v rámci systému, tj. WP
@@ -37,6 +44,34 @@ abstract class KT_Shortcode_Base implements KT_Registrable {
     public function getTag() {
         return $this->tag;
     }
+
+    /**
+     * Vrátí klíč tlačítka, pod kterým je/bude shortcode definován v editoru, pokud byl zadán při definici
+     * Pozn. registrace tlačítka v editoru je možná, jen když je zadán klíč i cesta skriptu pro tlačítko v editoru
+     * 
+     * @author Martin Hlaváč
+     * @link http://www.ktstudio.cz
+     * 
+     * @return string
+     */
+    public function getButtonKey() {
+        return $this->buttonKey;
+    }
+
+    /**
+     * Vrátí cestu skriptu tlačítka, pro editor, pokud byla zadána při definici
+     * Pozn. registrace tlačítka v editoru je možná, jen když je zadán klíč i cesta skriptu pro tlačítko v editoru
+     * 
+     * @author Martin Hlaváč
+     * @link http://www.ktstudio.cz
+     * 
+     * @return string
+     */
+    public function getButtonScriptPath() {
+        return $this->buttonScriptPath;
+    }
+
+    // --- veřejné metody ---------------------------
 
     /**
      * Událost zpracovávající vyvolání shortcodu v rámci WP
@@ -56,6 +91,60 @@ abstract class KT_Shortcode_Base implements KT_Registrable {
      */
     public function register() {
         add_shortcode($this->getTag(), array(&$this, "handler"));
+        $buttonKey = $this->getButtonKey();
+        $buttonScriptPath = $this->getButtonScriptPath();
+        if (kt_isset_and_not_empty($buttonKey) && kt_isset_and_not_empty($buttonScriptPath)) {
+            add_action("admin_init", array($this, "action_admin_init"));
+        }
+    }
+
+    // --- hooky a filtry ---------------------------
+
+    /**
+     * Akce pro inicializaci tlačítko shortcodu v editoru
+     * Pozn.: NENÍ POTŘEBA VOLAT VEŘEJNĚ
+     * 
+     * @author Martin Hlaváč
+     * @link http://www.ktstudio.cz
+     */
+    public function action_admin_init() {
+        if (current_user_can("edit_posts") && current_user_can("edit_pages")) {
+            add_filter("mce_buttons", array($this, "editorButtonFilter"));
+            add_filter("mce_external_plugins", array($this, "editorButtonPlugin"));
+        }
+    }
+
+    /**
+     * Filtr pro tlačítko shortcodu v editoru
+     * Pozn.: NENÍ POTŘEBA VOLAT VEŘEJNĚ
+     * 
+     * @author Martin Hlaváč
+     * @link http://www.ktstudio.cz
+     * 
+     * @param array $buttons
+     * @return type
+     */
+    public function editorButtonFilter(array $buttons) {
+        $buttonKey = $this->getButtonKey();
+        array_push($buttons, "|", $buttonKey);
+        return $buttons;
+    }
+
+    /**
+     * Filtr pro skript tlačítka shortcodu v editoru
+     * Pozn.: NENÍ POTŘEBA VOLAT VEŘEJNĚ
+     * 
+     * @author Martin Hlaváč
+     * @link http://www.ktstudio.cz
+     * 
+     * @param array $plugins
+     * @return array
+     */
+    public function editorButtonPlugin(array $plugins) {
+        $buttonKey = $this->getButtonKey();
+        $buttonScriptPath = $this->getButtonScriptPath();
+        $plugins[$buttonKey] = $buttonScriptPath;
+        return $plugins;
     }
 
 }
