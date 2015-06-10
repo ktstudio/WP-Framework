8312bb19cb0596f85cdb674f9a43ef20f35bdd3c
Tomáš Kocifaj <kocifaj@ktstudio.cz>
2015-06-10 08:23:56 +0200

    Oprava řazení číselníkuk přes Ajax a Sortable;

--- core/js/kt-core.js
+++ core/js/kt-core.js
@@ -150,14 +150,7 @@ jQuery(document).ready(function () {
             toggle.removeClass('off');
             input.attr('value', '1');
         }
-    }
-    
-    if(jQuery("table[data-sortable='true'] tbody").lenght > 0){
-        jQuery("table[data-sortable='true'] tbody").sortable({
-            helper: sortableTablefixHelper,
-            stop: sortableTableSave
-        }).disableSelection();
-    }
+    }   
 
     // Sortable číselníku vycházející z KT_Catalog_Model_Base
     var sortableTablefixHelper = function (e, ui) {
@@ -184,6 +177,13 @@ jQuery(document).ready(function () {
 
         jQuery.post(ajaxurl, data);
     };  
+    
+    if(jQuery("table[data-sortable='true'] tbody").length > 0){
+        jQuery("table[data-sortable='true'] tbody").sortable({
+            helper: sortableTablefixHelper,
+            stop: sortableTableSave,
+        }).disableSelection();
+    }
 
     // Slider input - jQuery UI
     
