1571c6fb0f071c8c857a2dbde0d1786304cfd068
Martin Hlaváč <hlavacm@hotmail.com>
2015-11-02 13:27:10 +0100

    Přidání obecné metody KT::curlGetContents

--- core/classes/kt.inc.php
+++ core/classes/kt.inc.php
@@ -1289,6 +1289,31 @@ class KT {
         return null;
     }
 
+    // --- cURL ---------------------------
+
+    /**
+     * Zpracování URL (callu) obecně pomocí cURL
+     * 
+     * @author Martin Hlaváč
+     * @link http://www.ktstudio.cz
+     *  
+     * @param string $url
+     * @return string
+     */
+    public static function curlGetContents($url) {
+        if (self::issetAndNotEmpty($url)) {
+            $curl = curl_init($url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+            $data = curl_exec($curl);
+            curl_close($curl);
+            return $data;
+        }
+        return null;
+    }
+
     // --- TEMPLATE LOAD ---------------------------
 
     /**
