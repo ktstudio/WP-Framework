fa6fc68252a7ccb00b4277adc7c0035ffd9595a2
Martin Hlaváč <hlavacm@hotmail.com>
2016-01-18 10:46:36 +0100

    Oprava automatického založení tabulky kt_termmeta

--- core/requires/functions/kt_sql_functions.inc.php
+++ core/requires/functions/kt_sql_functions.inc.php
@@ -28,16 +28,14 @@ function kt_sql_initialize_database() {
     }
     $termmetaTableName = "kt_termmeta";
     if (!kt_sql_create_sql($termmetaTableName, "
-            CREATE TABLE `{$termmetaTableName}` (
-            `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-            `level_id` int(5) unsigned NOT NULL,
-            `scope` varchar(30) DEFAULT NULL,
-            `message` text NOT NULL,
-            `date` datetime NOT NULL,
-            `logged_user_name` varchar(60) DEFAULT NULL,
-            `file` varchar(300) DEFAULT NULL,
-            `line` int(15) unsigned DEFAULT NULL,
-            PRIMARY KEY (`id`)
+            CREATE TABLE IF NOT EXISTS `{$termmetaTableName}` (
+              `meta_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+              `ktterm_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+              `meta_key` varchar(255) DEFAULT NULL,
+              `meta_value` longtext,
+              PRIMARY KEY (`meta_id`),
+              KEY `ktterm_id` (`ktterm_id`),
+              KEY `meta_key` (`meta_key`(191))
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_general_ci;
             ")) {
         $wasError = true;
