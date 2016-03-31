559608c11b4110f3d3ec527ca0a4aafd0d13e377
Martin Hlaváč <hlavacm@hotmail.com>
2016-03-31 09:36:31 +0200

    Zrušení výchozí auto sanitizace, resp. filtru na KT_WP_Editor_Fieldu

--- core/classes/fields/kt_wp_editor_field.inc.php
+++ core/classes/fields/kt_wp_editor_field.inc.php
@@ -1,43 +1,43 @@
 <?php
 
-class KT_WP_Editor_Field extends KT_Field{
-    
+class KT_WP_Editor_Field extends KT_Field {
+
     const FIELD_TYPE = "wp-editor";
-    
+
     public function __construct($name, $label) {
         parent::__construct($name, $label);
+        $this->setFilterSanitize(FILTER_DEFAULT);
     }
-    
-    // --- gettery a settery ---
-    
+
+    // --- gettery a settery ---------------------------
+
+    public function getFieldType() {
+        return self::FIELD_TYPE;
+    }
+
+    protected function getNameAttribute() {
+        $afterNameString = static::getAfterNameValue();
+        if (KT::issetAndNotEmpty($this->getPostPrefix())) {
+            return "{$this->getPostPrefix()}[{$this->getName()}]$afterNameString";
+        } else {
+            return "{$this->getName()}$afterNameString";
+        }
+    }
+
     public function getField() {
         $this->renderField();
     }
-    
+
+    // --- veřejné metody ---------------------------
+
     public function renderField() {
-        wp_editor( $this->getValue(), $this->getName(), array(
-		'media_buttons' => false,
-		'textarea_name' => $this->getNameAttribute(),
-		'textarea_rows' => 10,
-		'teeny' => false,
-		'quicktags' => false
-	));
+        wp_editor($this->getValue(), $this->getName(), array(
+            "media_buttons" => false,
+            "textarea_name" => $this->getNameAttribute(),
+            "textarea_rows" => 10,
+            "teeny" => false,
+            "quicktags" => false
+        ));
     }
-    
-    public function getFieldType(){
-        return self::FIELD_TYPE;
-    }
-    
-    protected function getNameAttribute() {
-        $html = "";
-        $afterNameString = static::getAfterNameValue();
-        
-        if (KT::issetAndNotEmpty($this->getPostPrefix())) {
-            $html .= "{$this->getPostPrefix()}[{$this->getName()}]$afterNameString";
-        } else {
-            $html .= "{$this->getName()}$afterNameString";
-        }
-        
-        return $html;
-    }
+
 }
