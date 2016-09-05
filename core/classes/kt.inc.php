e54a2dd2c9c718bac9f17ab6535d207ec7e53aef
Jan Pokorný <johnygemityg@gmail.com>
2016-09-05 09:24:44 +0200

    Úprava KT - nová metoda getTemplatePart

--- core/classes/kt.inc.php
+++ core/classes/kt.inc.php
@@ -1771,6 +1771,22 @@ class KT {
         return false;
     }
 
+    /**
+     * @author Jan Pokorný
+     * @param string $partName
+     * @param array $args Pole proměných dostupných v template partě
+     */
+    public static function getTemplatePart($partName, $args = []) {
+        $template = TEMPLATEPATH . '/' . $partName . '.php';
+        global $posts, $post, $wp_did_header, $wp_query, $wp_rewrite, $wpdb, $wp_version, $wp, $id, $comment, $user_ID;
+
+        if (is_array($wp_query->query_vars)) {
+            extract($wp_query->query_vars, EXTR_SKIP);
+        }
+        extract($args);
+        require_once $template;
+    }
+
     // --- TERMS ---------------------
 
     /**
