c560bec9500762283506d7a0f6f10a7c3d6de052
Martin Hlaváč <hlavacm@hotmail.com>
2018-08-15 09:41:30 +0200

    Úprava aplikace $currentUser v rámci KT_User_Profile_Config
    -> už není povinný, tj. možné použít i v případě nepřihlášeného uživatele

--- core/configs/kt_user_profile_config.inc.php
+++ core/configs/kt_user_profile_config.inc.php
@@ -2,7 +2,7 @@
 
 /**
  * Základní config, pomocí kterého je možné sestavit formulář uživatelského profilu
- * 
+ *
  * @author Martin Hlaváč
  * @link http://www.ktstudio.cz
  */
@@ -23,40 +23,45 @@ class KT_User_Profile_Config {
         $fieldset = new KT_Form_Fieldset(self::USER_PROFILE_FIELDSET);
         $fieldset->setPostPrefix(self::USER_PROFILE_FIELDSET);
 
-        if ($currentUser === null) {
-            $currentUser = wp_get_current_user();
-        }
+        $firstNameField = $fieldset->addText(self::FIRST_NAME, __("First name*:", "KT_CORE_DOMAIN"))
+            ->setAttrMaxlength(30)
+            ->addRule(KT_Field_Validator::REQUIRED, __("First name is required.", "KT_CORE_DOMAIN"))
+            ->addRule(KT_Field_Validator::MAX_LENGTH, __("The first name can be up to 30 characters.", "KT_CORE_DOMAIN"), 30);
+        $lastNameField = $fieldset->addText(self::LAST_NAME, __("Last name*:", "KT_CORE_DOMAIN"))
+            ->setAttrMaxlength(30)
+            ->addRule(KT_Field_Validator::REQUIRED, __("Last name is required.", "KT_CORE_DOMAIN"))
+            ->addRule(KT_Field_Validator::MAX_LENGTH, __("The last name can be up to 30 characters.", "KT_CORE_DOMAIN"), 30);
+        $emailField = $fieldset->addText(self::EMAIL, __("E-mail*:", "KT_CORE_DOMAIN"))
+            ->setAttrMaxlength(50)
+            ->addRule(KT_Field_Validator::REQUIRED, __("E-mail is required.", "KT_CORE_DOMAIN"))
+            ->addRule(KT_Field_Validator::EMAIL, __("Invalid e-mail address.", "KT_CORE_DOMAIN"))
+            ->addRule(KT_Field_Validator::MAX_LENGTH, __("The e-mail address can be up to 50 characters.", "KT_CORE_DOMAIN"), 50);
 
-        $fieldset->addText(self::FIRST_NAME, __("First name*:", "KT_CORE_DOMAIN"))
-                ->setDefaultValue($currentUser->user_firstname)
-                ->setAttrMaxlength(30)
-                ->addRule(KT_Field_Validator::REQUIRED, __("First name is required.", "KT_CORE_DOMAIN"))
-                ->addRule(KT_Field_Validator::MAX_LENGTH, __("The first name can be up to 30 characters.", "KT_CORE_DOMAIN"), 30);
-        $fieldset->addText(self::LAST_NAME, __("Last name*:", "KT_CORE_DOMAIN"))
-                ->setDefaultValue($currentUser->user_lastname)
-                ->setAttrMaxlength(30)
-                ->addRule(KT_Field_Validator::REQUIRED, __("Last name is required.", "KT_CORE_DOMAIN"))
-                ->addRule(KT_Field_Validator::MAX_LENGTH, __("The last name can be up to 30 characters.", "KT_CORE_DOMAIN"), 30);
-        $fieldset->addText(self::EMAIL, __("E-mail*:", "KT_CORE_DOMAIN"))
-                ->setDefaultValue($currentUser->user_email)
-                ->setAttrMaxlength(50)
-                ->addRule(KT_Field_Validator::REQUIRED, __("E-mail is required.", "KT_CORE_DOMAIN"))
-                ->addRule(KT_Field_Validator::EMAIL, __("Invalid e-mail address.", "KT_CORE_DOMAIN"))
-                ->addRule(KT_Field_Validator::MAX_LENGTH, __("The e-mail address can be up to 50 characters.", "KT_CORE_DOMAIN"), 50);
-        $userPhoneKey = KT_User_Profile_Config::PHONE;
         if ($withPhone) {
             $phoneLabel = ($isPhoneRequired) ? __("Phone*:", "KT_CORE_DOMAIN") : __("Phone:", "KT_CORE_DOMAIN");
             $phoneField = $fieldset->addText(self::PHONE, $phoneLabel)
-                    ->setDefaultValue($currentUser->$userPhoneKey)
-                    ->setAttrMaxlength(16)
-                    ->setPlaceholder(__("+420 606 707 808", "KT_CORE_DOMAIN"))
-                    ->setToolTip(__("The phone should be in international form, for example: \"+420 606 707 808\"...", "KT_CORE_DOMAIN"))
-                    ->addRule(KT_Field_Validator::MAX_LENGTH, __("The phone can have a maximum of 16 characters.", "KT_CORE_DOMAIN"), 16);
+                ->setAttrMaxlength(16)
+                ->setPlaceholder(__("+420 606 707 808", "KT_CORE_DOMAIN"))
+                ->setToolTip(__("The phone should be in international form, for example: \"+420 606 707 808\"...", "KT_CORE_DOMAIN"))
+                ->addRule(KT_Field_Validator::MAX_LENGTH, __("The phone can have a maximum of 16 characters.", "KT_CORE_DOMAIN"), 16);
             if ($isPhoneRequired) {
                 $phoneField->addRule(KT_Field_Validator::REQUIRED, __("Phone is required.", "KT_CORE_DOMAIN"));
             }
         }
         $fieldset->addWpNonce(self::NONCE);
+
+        if ($currentUser === null && is_user_logged_in()) {
+            $currentUser = wp_get_current_user();
+        }
+        if (KT::issetAndNotEmpty($currentUser)) {
+            $firstNameField->setDefaultValue($currentUser->user_firstname);
+            $lastNameField->setDefaultValue($currentUser->user_lastname);
+            $emailField->setDefaultValue($currentUser->user_email);
+            if ($withPhone) {
+                $userPhoneKey = KT_User_Profile_Config::PHONE;
+                $phoneField->setDefaultValue($currentUser->$userPhoneKey);
+            }
+        }
 
         return $fieldset;
     }
@@ -65,16 +70,16 @@ class KT_User_Profile_Config {
         $fieldset = self::getSimpleProfileFieldset($currentUser, $withPhone, $isPhoneRequired);
 
         $fieldset->addText(self::PASSWORD, __("Password*:", "KT_CORE_DOMAIN"))
-                ->setInputType(KT_Text_Field::INPUT_PASSWORD)
-                ->setAttrMaxlength(20)
-                ->addRule(KT_Field_Validator::MIN_LENGTH, __("The password can have a minimum of 7 characters.", "KT_CORE_DOMAIN"), 7)
-                ->addRule(KT_Field_Validator::MAX_LENGTH, __("The password can have a maximum of 30 characters.", "KT_CORE_DOMAIN"), 30);
+            ->setInputType(KT_Text_Field::INPUT_PASSWORD)
+            ->setAttrMaxlength(20)
+            ->addRule(KT_Field_Validator::MIN_LENGTH, __("The password can have a minimum of 7 characters.", "KT_CORE_DOMAIN"), 7)
+            ->addRule(KT_Field_Validator::MAX_LENGTH, __("The password can have a maximum of 30 characters.", "KT_CORE_DOMAIN"), 30);
         $fieldset->addText(self::PASSWORD_CONFIRM, __("Password confirmation*:", "KT_CORE_DOMAIN"))
-                ->setInputType(KT_Text_Field::INPUT_PASSWORD)
-                ->setAttrMaxlength(20)
-                ->addRule(KT_Field_Validator::MIN_LENGTH, __("The password confirmation can have a minimum of 7 characters.", "KT_CORE_DOMAIN"), 7)
-                ->addRule(KT_Field_Validator::MAX_LENGTH, __("The password confirmation can have a maximum of 30 characters.", "KT_CORE_DOMAIN"), 30)
-                ->setTooltip(__("Type again your new password.", "KT_CORE_DOMAIN"));
+            ->setInputType(KT_Text_Field::INPUT_PASSWORD)
+            ->setAttrMaxlength(20)
+            ->addRule(KT_Field_Validator::MIN_LENGTH, __("The password confirmation can have a minimum of 7 characters.", "KT_CORE_DOMAIN"), 7)
+            ->addRule(KT_Field_Validator::MAX_LENGTH, __("The password confirmation can have a maximum of 30 characters.", "KT_CORE_DOMAIN"), 30)
+            ->setTooltip(__("Type again your new password.", "KT_CORE_DOMAIN"));
 
         return $fieldset;
     }
@@ -89,22 +94,22 @@ class KT_User_Profile_Config {
         $fieldset->setPostPrefix(self::PASSWORD_FIELDSET);
 
         $fieldset->addText(self::PASSWORD_OLD, __("Current password*:", "KT_CORE_DOMAIN"))
-                ->setInputType(KT_Text_Field::INPUT_PASSWORD)
-                ->setAttrMaxlength(20)
-                ->addRule(KT_Field_Validator::MIN_LENGTH, __("The current password can have a minimum of 7 characters.", "KT_CORE_DOMAIN"), 7)
-                ->addRule(KT_Field_Validator::MAX_LENGTH, __("The current password can have a maximum of 30 characters.", "KT_CORE_DOMAIN"), 30);
+            ->setInputType(KT_Text_Field::INPUT_PASSWORD)
+            ->setAttrMaxlength(20)
+            ->addRule(KT_Field_Validator::MIN_LENGTH, __("The current password can have a minimum of 7 characters.", "KT_CORE_DOMAIN"), 7)
+            ->addRule(KT_Field_Validator::MAX_LENGTH, __("The current password can have a maximum of 30 characters.", "KT_CORE_DOMAIN"), 30);
 
         $fieldset->addText(self::PASSWORD_NEW, __("New password*:", "KT_CORE_DOMAIN"))
-                ->setInputType(KT_Text_Field::INPUT_PASSWORD)
-                ->setAttrMaxlength(20)
-                ->addRule(KT_Field_Validator::MIN_LENGTH, __("The new password can have a minimum of 7 characters.", "KT_CORE_DOMAIN"), 7)
-                ->addRule(KT_Field_Validator::MAX_LENGTH, __("The new password can have a maximum of 30 characters.", "KT_CORE_DOMAIN"), 30);
+            ->setInputType(KT_Text_Field::INPUT_PASSWORD)
+            ->setAttrMaxlength(20)
+            ->addRule(KT_Field_Validator::MIN_LENGTH, __("The new password can have a minimum of 7 characters.", "KT_CORE_DOMAIN"), 7)
+            ->addRule(KT_Field_Validator::MAX_LENGTH, __("The new password can have a maximum of 30 characters.", "KT_CORE_DOMAIN"), 30);
         $fieldset->addText(self::PASSWORD_CONFIRM, __("Password confirmation*:", "KT_CORE_DOMAIN"))
-                ->setInputType(KT_Text_Field::INPUT_PASSWORD)
-                ->setAttrMaxlength(20)
-                ->addRule(KT_Field_Validator::MIN_LENGTH, __("The password confirmation can have a minimum of 7 characters.", "KT_CORE_DOMAIN"), 7)
-                ->addRule(KT_Field_Validator::MAX_LENGTH, __("The password confirmation can have a maximum of 30 characters.", "KT_CORE_DOMAIN"), 30)
-                ->setTooltip(__("Type again your new password.", "KT_CORE_DOMAIN"));
+            ->setInputType(KT_Text_Field::INPUT_PASSWORD)
+            ->setAttrMaxlength(20)
+            ->addRule(KT_Field_Validator::MIN_LENGTH, __("The password confirmation can have a minimum of 7 characters.", "KT_CORE_DOMAIN"), 7)
+            ->addRule(KT_Field_Validator::MAX_LENGTH, __("The password confirmation can have a maximum of 30 characters.", "KT_CORE_DOMAIN"), 30)
+            ->setTooltip(__("Type again your new password.", "KT_CORE_DOMAIN"));
 
         return $fieldset;
     }
