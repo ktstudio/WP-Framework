241b826c44ad73c80f5a01fe7442e9e8b32774f6
Jakub Jetleb <jakubjetleb@gmail.com>
2019-11-27 15:15:06 +0100

    Cookies bar rendering change

--- core/requires/functions/kt_general_functions.inc.php
+++ core/requires/functions/kt_general_functions.inc.php
@@ -96,8 +96,8 @@ function kt_wp_before_admin_bar_render_callback() {
     ));
 }
 
-add_action("wp_ajax_kt_load_cookie_statement_content", "kt_load_cookie_statement_content_callback");
-add_action("wp_ajax_nopriv_kt_load_cookie_statement_content", "kt_load_cookie_statement_content_callback");
+add_action("wp_footer", "kt_load_cookie_statement_content_callback");
+
 
 /**
  * Funce zajistí načtení, resp. vrácení cookie statement obsahu ajaxem
@@ -105,8 +105,9 @@ add_action("wp_ajax_nopriv_kt_load_cookie_statement_content", "kt_load_cookie_st
  * @author Martin Hlaváč
  * @link http://www.ktstudio.cz
  */
-function kt_load_cookie_statement_content_callback() {
-    die(KT_WP_Configurator::getCookieStatementHtml());
+function kt_load_cookie_statement_content_callback()
+{
+    echo KT_WP_Configurator::getCookieStatementHtml();
 }
 
 // dynamicke fieldy
